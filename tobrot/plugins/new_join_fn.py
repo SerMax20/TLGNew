cc092825723e89394fcc6faaa855dd9ef180ee3b
SerMax20 <67185027+SerMax20@users.noreply.github.com>
2020-07-15 12:11:08 +0000

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -33,25 +33,25 @@ async def new_join_f(client, message):
 
 
 async def help_message_f(client, message):
-    # await message.reply_text("no one gonna help you 🤣🤣🤣🤣", quote=True)
-    #channel_id = str(AUTH_CHANNEL)[4:]
-    #message_id = 99
-    # display the /help
+     await message.reply_text("🙏 Welcome...!", quote=True)
+     channel_id = str(AUTH_CHANNEL)[4:]
+     message_id = 99
+     display the /help
     
-    await message.reply_text("""join this group forr help-- @torrentleechgdrivesupport\n\n And also don't forget to fork this repo: <a href="https://github.com/gautamajay52/TorrentLeech-Gdrive">TorrentLeech-Gdrive</a>""", disable_web_page_preview=True)
+    await message.reply_text("""<b>Need Any Help? Then Contact me</b>--𝙿𝚕𝚎𝚊𝚜𝚎 𝚁𝚎𝚊𝚍 𝙿𝚒𝚗𝚗𝚎𝚍 𝙼𝚎𝚜𝚜𝚊𝚐𝚎\n\n<b> and also Don't forget to Subscribe Our Channel</b>: <a href="https://t.me/">JOIN CHANNEL""", disable_web_page_preview=True)
 
 
 async def rename_message_f(client, message):
     inline_keyboard = []
     inline_keyboard.append([
         pyrogram.InlineKeyboardButton(
-            text="read this?",
-            url="https://t.me/keralagram/698909"
+            text="🙏 Welcome...!",
+            url="https://t.me/"
         )
     ])
     reply_markup = pyrogram.InlineKeyboardMarkup(inline_keyboard)
     await message.reply_text(
-        "please use @renamebot",
+        "🌀Thank you !",
         quote=True,
         reply_markup=reply_markup
     )
